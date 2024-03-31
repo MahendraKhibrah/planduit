86e15e7c6c9ea20594125cc84d7ab4fe78334761
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-03-31 22:51:51 +0700

    v1.0.0 build 6 candidate

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions{
     const val versionMajor = 1
     const val versionMedium = 0
     const val versionMinor = 0
-    const val buildNum = 5
+    const val buildNum = 6
 
     val versionCode
         get() = 100000000 + (
